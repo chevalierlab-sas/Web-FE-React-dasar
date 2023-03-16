af7b98ee46414f0b8808bd0cd56428a8272642d2
Rama Sanjaya <ramasanjaya2202@gmail.com>
2023-03-16 14:16:19 +0700

    menambahkan addNote

--- src/components/AddNote.jsx
+++ src/components/AddNote.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
 
 function AddNote() {
   return (
-    <div>AddNote</div>
-  )
+    <div>
+      <div className="card w-auto bg-base-100 shadow-xl mx-auto my-4 shadow-md">
+          <h2 className="card-title p-4 font-semibold">Buat Catatan</h2>
+        <div className="card-body p-4">
+          <form className="form-control">
+            <input className="input w-full my-4" placeholder="Masukkan Judul" type={"text"} required />
+            <textarea className="textarea textarea-lg w-full my-4" placeholder="Isi catatan" type={"text"} required />
+            <button className="btn btn-block" type="submit">Simpan</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default AddNote
+export default AddNote;
